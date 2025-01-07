bdee191002e33a28e42b5ef1376e421f2c25ee7b
benjaminezequiel <contato.benjaminezequiel@gmail.com>
2025-01-06 23:20:37 -0300

    fix: remove unecessary logs

--- src/components/Heatmap.tsx
+++ src/components/Heatmap.tsx
@@ -45,8 +45,6 @@ const HeatmapCell = ({
   const [year, month, day] = date.split('-').map(Number);
   const localDate = new Date(year, month - 1, day);
 
-  console.log(localDate)
-
   return (
     <Tooltip>
       <TooltipTrigger asChild>
